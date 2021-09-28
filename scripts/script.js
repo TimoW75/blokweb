7c827ad8f10abda2784593937861c0b4e4e5780a
Timowerkhoven <90208921+Timowerkhoven@users.noreply.github.com>
2021-09-28 23:02:27 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
 // JavaScript Document
 var deButton = document.querySelector("header img:first-of-type");
 var deNav = document.querySelector("nav")
-var Main = document.querySelector("header")
+
 
 deButton.addEventListener("click", toggleMenu);
 
 function toggleMenu(event) {
   deNav.classList.toggle("toonMenu");
-  Main.classList.toggle("toonMenu");
+  deButton.classList.toggle("toonMenu");
 }
